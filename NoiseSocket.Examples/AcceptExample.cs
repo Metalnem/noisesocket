587ca5024af27e835323cc0f378735379a4b8ab3
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-04-25 16:23:10 +0200

    Document the examples

--- NoiseSocket.Examples/AcceptExample.cs
+++ NoiseSocket.Examples/AcceptExample.cs
@@ -9,10 +9,13 @@ namespace Noise.Examples
 	public static class AcceptExample
 	{
 		private const int Port = 19285;
+
+		// Pad all the messages to 117 bytes to hide the plaintext length.
 		private const int PaddedLength = 117;
 
+		// The agreed protocol between the client and the server. In the real world it would
+		// be somehow encoded in the negotiation data in the initial handshake message.
 		private static readonly Protocol protocol = Protocol.Parse("Noise_XX_25519_AESGCM_BLAKE2b".AsSpan());
-		private static readonly byte[] negotiationData = new byte[] { 0, 1, 1, 2, 2, 9 };
 
 		public static Task Run()
 		{
@@ -24,25 +27,39 @@ namespace Noise.Examples
 
 		private static async Task Client()
 		{
+			// Open the TCP connection to the server.
 			using (var client = new TcpClient())
 			{
 				await client.ConnectAsync(IPAddress.Loopback, Port);
 
+				// Generate the static key pair.
 				using (var keyPair = KeyPair.Generate())
 				{
 					var config = new ProtocolConfig(initiator: true, s: keyPair.PrivateKey);
 
+					// Initialize the NoiseSocket with the network stream.
 					using (var stream = client.GetStream())
 					using (var noise = new NoiseSocket(protocol, config, stream))
 					{
-						await noise.WriteHandshakeMessageAsync(negotiationData, paddedLength: PaddedLength);
+						// Send the initial handshake message to the server. In the real world the
+						// negotiation data would contain the initial protocol, supported protocols
+						// for the switch and retry cases, and maybe some other negotiation options.
+						await noise.WriteHandshakeMessageAsync(negotiationData: null, paddedLength: PaddedLength);
+
+						// Receive the negotiation data from the server. In this example we will
+						// assume that the server decided to accept the offered protocol.
 						await noise.ReadNegotiationDataAsync();
+
+						// Finish the handshake.
 						await noise.ReadHandshakeMessageAsync();
 						await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
 
+						// Send the padded transport message to the server.
 						var request = Encoding.UTF8.GetBytes("I'm cooking MC's like a pound of bacon");
 						await noise.WriteMessageAsync(request, PaddedLength);
 
+						// Receive the transport message from the
+						// server and print it to the standard output.
 						var response = await noise.ReadMessageAsync();
 						Console.WriteLine(Encoding.UTF8.GetString(response));
 					}
@@ -52,29 +69,44 @@ namespace Noise.Examples
 
 		private static async Task Server()
 		{
+			// Listen for connections from TCP network clients.
 			var listener = new TcpListener(IPAddress.Loopback, Port);
 			listener.Start();
 
 			try
 			{
+				// Accept the connection from the TCP client.
 				using (var client = await listener.AcceptTcpClientAsync())
-				using (var keyPair = KeyPair.Generate())
 				{
-					var config = new ProtocolConfig(initiator: false, s: keyPair.PrivateKey);
-
-					using (var stream = client.GetStream())
-					using (var noise = new NoiseSocket(protocol, config, stream))
+					// Generate the static key pair.
+					using (var keyPair = KeyPair.Generate())
 					{
-						await noise.ReadNegotiationDataAsync();
-						noise.Accept(protocol, config);
+						var config = new ProtocolConfig(initiator: false, s: keyPair.PrivateKey);
 
-						await noise.ReadHandshakeMessageAsync();
-						await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
-						await noise.ReadNegotiationDataAsync();
-						await noise.ReadHandshakeMessageAsync();
+						// Initialize the NoiseSocket with the network stream.
+						using (var stream = client.GetStream())
+						using (var noise = new NoiseSocket(protocol, config, stream))
+						{
+							// Receive the negotiation data from the client. In the real world the
+							// negotiation data would contain the initial protocol, supported protocols
+							// for the switch and retry cases, and maybe some other negotiation options.
+							await noise.ReadNegotiationDataAsync();
 
-						var request = await noise.ReadMessageAsync();
-						await noise.WriteMessageAsync(request, PaddedLength);
+							// Accept the offered protocol.
+							noise.Accept(protocol, config);
+
+							// Finish the handshake.
+							await noise.ReadHandshakeMessageAsync();
+							await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
+							await noise.ReadNegotiationDataAsync();
+							await noise.ReadHandshakeMessageAsync();
+
+							// Receive the transport message from the client.
+							var request = await noise.ReadMessageAsync();
+
+							// Echo the message back to the client.
+							await noise.WriteMessageAsync(request, PaddedLength);
+						}
 					}
 				}
 			}
--- NoiseSocket.Examples/SwitchExample.cs
+++ NoiseSocket.Examples/SwitchExample.cs
@@ -9,10 +9,13 @@ namespace Noise.Examples
 	public static class SwitchExample
 	{
 		private const int Port = 56473;
+
+		// Pad all the messages to 2048 bytes to hide the plaintext length.
 		private const int PaddedLength = 2048;
 
+		// The agreed protocol between the client and the server. In the real world it would
+		// be somehow encoded in the negotiation data in the initial handshake message.
 		private static readonly Protocol protocol = Protocol.Parse("Noise_XX_25519_AESGCM_BLAKE2b".AsSpan());
-		private static readonly byte[] negotiationData = new byte[] { 0, 1, 1, 2, 2, 9 };
 
 		public static Task Run()
 		{
@@ -24,65 +27,105 @@ namespace Noise.Examples
 
 		private static async Task Client()
 		{
+			// Connect to the server using the TCP socket.
 			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
 			{
 				await socket.ConnectAsync(IPAddress.Loopback, Port);
 
+				// Select the initial Noise protocol and configuration.
 				var initial = Protocol.Parse("Noise_NN_25519_ChaChaPoly_SHA256".AsSpan());
 				var config = new ProtocolConfig(initiator: true);
 
-				using (var keyPair = KeyPair.Generate())
+				// Initialize the NoiseSocket with the network stream.
 				using (var stream = new NetworkStream(socket))
 				using (var noise = new NoiseSocket(initial, config, stream))
 				{
-					await noise.WriteHandshakeMessageAsync(negotiationData, paddedLength: PaddedLength);
+					// Send the initial handshake message to the server. In the real world the
+					// negotiation data would contain the initial protocol, supported protocols
+					// for the switch and retry cases, and maybe some other negotiation options.
+					await noise.WriteHandshakeMessageAsync(negotiationData: null, paddedLength: PaddedLength);
+
+					// Receive the negotiation data from the server. In this example we will
+					// assume that the server decided to switch to Noise_XX_25519_AESGCM_BLAKE2b.
 					await noise.ReadNegotiationDataAsync();
 
-					config = new ProtocolConfig(initiator: false, s: keyPair.PrivateKey);
-					noise.Switch(protocol, config);
+					// New protocol requires static key, so it's generated here.
+					using (var keyPair = KeyPair.Generate())
+					{
+						// The client now plays the role of the responder.
+						config = new ProtocolConfig(initiator: false, s: keyPair.PrivateKey);
 
-					await noise.ReadHandshakeMessageAsync();
-					await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
-					await noise.ReadNegotiationDataAsync();
-					await noise.ReadHandshakeMessageAsync();
+						// Switch to a protocol different from the initial one.
+						noise.Switch(protocol, config);
 
-					var request = Encoding.UTF8.GetBytes("I'm cooking MC's like a pound of bacon");
-					await noise.WriteMessageAsync(request, PaddedLength);
+						// Finish the handshake using the new protocol.
+						await noise.ReadHandshakeMessageAsync();
+						await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
+						await noise.ReadNegotiationDataAsync();
+						await noise.ReadHandshakeMessageAsync();
 
-					var response = await noise.ReadMessageAsync();
-					Console.WriteLine(Encoding.UTF8.GetString(response));
+						// Send the padded transport message to the server.
+						var request = Encoding.UTF8.GetBytes("I'm cooking MC's like a pound of bacon");
+						await noise.WriteMessageAsync(request, PaddedLength);
+
+						// Receive the transport message from the
+						// server and print it to the standard output.
+						var response = await noise.ReadMessageAsync();
+						Console.WriteLine(Encoding.UTF8.GetString(response));
+					}
 				}
 			}
 		}
 
 		private static async Task Server()
 		{
+			// Listen for incoming TCP connections.
 			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
 			{
 				socket.Bind(new IPEndPoint(IPAddress.Loopback, Port));
 				socket.Listen((int)SocketOptionName.MaxConnections);
 
+				// Select the initial Noise protocol and configuration.
 				var initial = Protocol.Parse("Noise_IN_25519_AESGCM_BLAKE2s".AsSpan());
 				var config = new ProtocolConfig(initiator: false);
 
+				// Accept the connection and initialize the NoiseSocket with its network stream.
 				using (var client = await socket.AcceptAsync())
-				using (var keyPair = KeyPair.Generate())
 				using (var stream = new NetworkStream(client))
 				using (var noise = new NoiseSocket(initial, config, stream))
 				{
+					// Receive the negotiation data from the client. In the real world the
+					// negotiation data would contain the initial protocol, supported protocols
+					// for the switch and retry cases, and maybe some other negotiation options.
 					await noise.ReadNegotiationDataAsync();
 
-					config = new ProtocolConfig(initiator: true, s: keyPair.PrivateKey);
-					noise.Switch(protocol, config);
+					// New protocol requires static key, so we generate it here.
+					using (var keyPair = KeyPair.Generate())
+					{
+						// The server decides to switch to a new protocol.
+						// It now plays the role of the initiator.
+						config = new ProtocolConfig(initiator: true, s: keyPair.PrivateKey);
+						noise.Switch(protocol, config);
 
-					await noise.IgnoreHandshakeMessageAsync();
-					await noise.WriteHandshakeMessageAsync(negotiationData, paddedLength: PaddedLength);
-					await noise.ReadNegotiationDataAsync();
-					await noise.ReadHandshakeMessageAsync();
-					await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
+						// Read the handshake message from the client, but without decrypting it.
+						await noise.IgnoreHandshakeMessageAsync();
 
-					var request = await noise.ReadMessageAsync();
-					await noise.WriteMessageAsync(request, PaddedLength);
+						// Send the first handshake message using the new protocol. In the
+						// real world the negotiation data would encode the details about
+						// the server's desicion to switch protocol.
+						await noise.WriteHandshakeMessageAsync(negotiationData: null, paddedLength: PaddedLength);
+
+						// Finish the handshake using the new protocol.
+						await noise.ReadNegotiationDataAsync();
+						await noise.ReadHandshakeMessageAsync();
+						await noise.WriteHandshakeMessageAsync(paddedLength: PaddedLength);
+
+						// Receive the transport message from the client.
+						var request = await noise.ReadMessageAsync();
+
+						// Echo the message back to the client.
+						await noise.WriteMessageAsync(request, PaddedLength);
+					}
 				}
 			}
 		}
