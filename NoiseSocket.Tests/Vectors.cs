115c6363e3d8519198c6b28c809aeb4fc314a08d
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-05-04 19:06:51 +0200

    Performance improvements in vectorgen

--- NoiseSocket.Tests/Vectors.cs
+++ NoiseSocket.Tests/Vectors.cs
@@ -9,15 +9,28 @@ namespace Noise.Tests
 {
 	internal static class Vectors
 	{
-		private const string Prologue = "4a6f686e2047616c74";
-		private const string InitStatic = "e61ef9919cde45dd5f82166404bd08e38bceb5dfdfded0a34c8df7ed542214d1";
-		private const string InitStaticPublic = "6bc3822a2aa7f4e6981d6538692b3cdf3e6df9eea6ed269eb41d93c22757b75a";
-		private const string InitEphemeral = "893e28b9dc6ca8d611ab664754b8ceb7bac5117349a4439a6b0569da977c464a";
-		private const string RespStatic = "4a3acbfdb163dec651dfa3194dece676d437029c62a408b4c5ea9114246e4893";
-		private const string RespStaticPublic = "31e0303fd6418d2f8c0e78b91f22e8caed0fbe48656dcf4767e4834f701b8f62";
-		private const string RespEphemeral = "bbdb4cdbd309f1a1f2e1456967fe288cadd6f712d65dc7b7793d5e63da6b375b";
+		private const string PrologueHex = "4a6f686e2047616c74";
+		private static readonly byte[] PrologueRaw = Hex.Decode(PrologueHex);
 
-		private static readonly List<string> payloads = new List<string>
+		private const string InitStaticHex = "e61ef9919cde45dd5f82166404bd08e38bceb5dfdfded0a34c8df7ed542214d1";
+		private static readonly byte[] InitStaticRaw = Hex.Decode(InitStaticHex);
+
+		private const string InitStaticPublicHex = "6bc3822a2aa7f4e6981d6538692b3cdf3e6df9eea6ed269eb41d93c22757b75a";
+		private static readonly byte[] InitStaticPublicRaw = Hex.Decode(InitStaticPublicHex);
+
+		private const string InitEphemeralHex = "893e28b9dc6ca8d611ab664754b8ceb7bac5117349a4439a6b0569da977c464a";
+		private static readonly byte[] InitEphemeralRaw = Hex.Decode(InitEphemeralHex);
+
+		private const string RespStaticHex = "4a3acbfdb163dec651dfa3194dece676d437029c62a408b4c5ea9114246e4893";
+		private static readonly byte[] RespStaticRaw = Hex.Decode(RespStaticHex);
+
+		private const string RespStaticPublicHex = "31e0303fd6418d2f8c0e78b91f22e8caed0fbe48656dcf4767e4834f701b8f62";
+		private static readonly byte[] RespStaticPublicRaw = Hex.Decode(RespStaticPublicHex);
+
+		private const string RespEphemeralHex = "bbdb4cdbd309f1a1f2e1456967fe288cadd6f712d65dc7b7793d5e63da6b375b";
+		private static readonly byte[] RespEphemeralRaw = Hex.Decode(RespEphemeralHex);
+
+		private static readonly List<string> payloadsHex = new List<string>
 		{
 			"4c756477696720766f6e204d69736573",
 			"4d757272617920526f746862617264",
@@ -26,6 +39,8 @@ namespace Noise.Tests
 			"4a65616e2d426170746973746520536179",
 			"457567656e2042f6686d20766f6e2042617765726b"
 		};
+
+		private static readonly List<byte[]> payloadsRaw = payloadsHex.Select(Hex.Decode).ToList();
 
 		public static IEnumerable<Vector> Generate()
 		{
@@ -49,16 +64,16 @@ namespace Noise.Tests
 
 					var initConfig = new ProtocolConfig(
 						initiator: true,
-						prologue: Hex.Decode(Prologue),
-						s: hasInitStatic ? Hex.Decode(InitStatic) : null,
-						rs: hasInitRemoteStatic ? Hex.Decode(RespStaticPublic) : null
+						prologue: PrologueRaw,
+						s: hasInitStatic ? InitStaticRaw : null,
+						rs: hasInitRemoteStatic ? RespStaticPublicRaw : null
 					);
 
 					var respConfig = new ProtocolConfig(
 						initiator: false,
-						prologue: Hex.Decode(Prologue),
-						s: hasRespStatic ? Hex.Decode(RespStatic) : null,
-						rs: hasRespRemoteStatic ? Hex.Decode(InitStaticPublic) : null
+						prologue: PrologueRaw,
+						s: hasRespStatic ? RespStaticRaw : null,
+						rs: hasRespRemoteStatic ? InitStaticPublicRaw : null
 					);
 
 					var initSocket = NoiseSocket.CreateClient(protocol, initConfig, stream, true);
@@ -66,51 +81,37 @@ namespace Noise.Tests
 
 					var messages = new List<Message>();
 					var name = (byte[])protocolName.GetValue(protocol);
-					var negotiationData = name;
-					var accepted = false;
 
-					foreach (var payload in payloads)
+					for (int i = 0; i < payloadsHex.Count; ++i)
 					{
+						stream.Position = 0;
+
+						var hex = payloadsHex[i];
+						var raw = payloadsRaw[i];
+
 						if (initSocket.HandshakeHash.IsEmpty)
 						{
-							stream.Position = 0;
-							initSocket.WriteHandshakeMessageAsync(name, Hex.Decode(payload)).GetAwaiter().GetResult();
-							negotiationData = null;
-
-							var message = new Message
-							{
-								Payload = payload,
-								Ciphertext = ReadMessage(stream)
-							};
-
-							messages.Add(message);
+							byte[] negotiationData = i == 0 ? name : null;
+							initSocket.WriteHandshakeMessageAsync(negotiationData, raw).GetAwaiter().GetResult();
+							messages.Add(new Message { Payload = hex, Ciphertext = ReadMessage(stream) });
 
 							stream.Position = 0;
 							respSocket.ReadNegotiationDataAsync().GetAwaiter().GetResult();
 
-							if (!accepted)
+							if (i == 0)
 							{
 								respSocket.Accept(protocol, respConfig);
-								accepted = true;
 							}
 
 							respSocket.ReadHandshakeMessageAsync().GetAwaiter().GetResult();
 						}
 						else
 						{
-							stream.Position = 0;
-							initSocket.WriteMessageAsync(Hex.Decode(payload)).GetAwaiter().GetResult();
+							initSocket.WriteMessageAsync(raw).GetAwaiter().GetResult();
+							messages.Add(new Message { Payload = hex, Ciphertext = ReadMessage(stream) });
 
 							stream.Position = 0;
 							respSocket.ReadMessageAsync().GetAwaiter().GetResult();
-
-							var message = new Message
-							{
-								Payload = payload,
-								Ciphertext = ReadMessage(stream)
-							};
-
-							messages.Add(message);
 						}
 
 						var temp = initSocket;
@@ -121,14 +122,14 @@ namespace Noise.Tests
 					var vector = new Vector
 					{
 						ProtocolName = Encoding.ASCII.GetString(name),
-						InitPrologue = Prologue,
-						InitStatic = hasInitStatic ? InitStatic : null,
-						InitEphemeral = InitEphemeral,
-						InitRemoteStatic = hasInitRemoteStatic ? RespStaticPublic : null,
-						RespPrologue = Prologue,
-						RespStatic = hasRespStatic ? RespStatic : null,
-						RespEphemeral = RespEphemeral,
-						RespRemoteStatic = hasRespRemoteStatic ? InitStaticPublic : null,
+						InitPrologue = PrologueHex,
+						InitStatic = hasInitStatic ? InitStaticHex : null,
+						InitEphemeral = InitEphemeralHex,
+						InitRemoteStatic = hasInitRemoteStatic ? RespStaticPublicHex : null,
+						RespPrologue = PrologueHex,
+						RespStatic = hasRespStatic ? RespStaticHex : null,
+						RespEphemeral = RespEphemeralHex,
+						RespRemoteStatic = hasRespRemoteStatic ? InitStaticPublicHex : null,
 						HandshakeHash = Hex.Encode(initSocket.HandshakeHash.ToArray()),
 						Messages = messages
 					};
